f0da92a28e8d87a05fcc8ae69c966034089b4097
Allan Salgado <allan.salgado3@gmail.com>
2024-11-18 15:42:07 -0600

    Changes

--- ticolinea.stream.service/Constantes/Global.cs
+++ ticolinea.stream.service/Constantes/Global.cs
@@ -5,7 +5,7 @@
 #if !DEBUG
         public const string STREAMS_FOLDER = "/home/ticolineaplay/streams/";
         public const string EPG_FOLDER = "/home/ticolineaplay/EPG/";
-        public const string FFMPEG_PATH_SRT = "/home/ticolineaplay/ffmpeg/ffmpeg";
+        public const string FFMPEG_PATH_SRT = "ffmpeg";
         public const string FFMPEG_PATH = "ffmpeg";
         public const string FFPROBE_PATH = "/home/ticolineaplay/tools/ffprobe";
         public const string MARIADB_CONN= "server=127.0.0.1;Port=3306;uid=iptvtl;pwd=Qawsedrf7852!;database=iptv;Allow User Variables=True;SSLMode=None;Pooling=true;Min Pool Size=25;Max Pool Size=350;Connection Lifetime=0;AllowPublicKeyRetrieval=true";
